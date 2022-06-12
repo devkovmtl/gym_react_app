bb8945c5cc7edb04cb95c27895a84a2351764d57
Mikael <dev.kov.mtl@gmail.com>
2022-06-12 14:34:27 -0400

    Home Page - Search Exercise

--- src/components/SearchExercises.js
+++ src/components/SearchExercises.js
@@ -1,4 +1,62 @@
+import { useState, useEffect } from 'react';
+import { Box, Button, Stack, TextField, Typography } from '@mui/material';
+
 const SearchExercises = () => {
-  return <div>SearchExercises</div>;
+  const [search, setSearch] = useState('');
+
+  const handleSearch = async () => {
+    if (search) {
+      // const exerciseData = await fetchData()
+    }
+  };
+
+  return (
+    <Stack alignItems='center' mt='37px' justifyContent='center' p='20px'>
+      <Typography
+        fontWeight={700}
+        sx={{
+          fontSize: { lg: '44px', xs: '30px' },
+        }}
+        mb='50px'
+        textAlign='center'
+      >
+        Awesome Exercises You Should Know
+        <br />
+      </Typography>
+      <Box position='relative' mb='72px'>
+        <TextField
+          height='76px'
+          value={search}
+          onChange={(e) => {
+            setSearch(e.target.value.toLowerCase());
+          }}
+          placeholder='Search Exercises'
+          type='text'
+          sx={{
+            input: { fontWeight: '600', border: 'none', borderRadius: '4px' },
+            width: { lg: '800px', xs: '350px' },
+            backgroundColor: '#fff',
+            borderRadius: '40px',
+          }}
+        />
+        <Button
+          className='search-btn'
+          sx={{
+            bgcolor: '#FF2625',
+            color: '#FFF',
+            textTransform: 'none',
+            width: { lg: '175px', xs: '80px' },
+            fontSize: { lg: '20px', xs: '12px' },
+            height: '56px',
+            position: 'absolute',
+            right: '0',
+          }}
+          onClick={handleSearch}
+        >
+          Search
+        </Button>
+      </Box>
+    </Stack>
+  );
 };
 export default SearchExercises;
