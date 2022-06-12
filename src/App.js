3e7937e386307df7a597c8c47df4f10dd6eae05c
Mikael <dev.kov.mtl@gmail.com>
2022-06-12 12:54:44 -0400

    Project Structure

--- src/App.js
+++ src/App.js
@@ -1,6 +1,19 @@
+import { Routes, Route } from 'react-router-dom';
+import { Box } from '@mui/material';
+import { Footer, Navbar } from './components';
+import { Home, ExerciseDetail } from './pages';
 import './App.css';
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <Box width='400px' sx={{ width: { xl: '1488px' } }} m='auto'>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/exercise/:id' element={<ExerciseDetail />} />
+      </Routes>
+      <Footer />
+    </Box>
+  );
 };
 export default App;
